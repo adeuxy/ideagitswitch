4b08f631961da4455805caa69c7414ebc8a9e582
Charlie <charlie@example.com>
2025-07-17 09:23:27 +0800

    v1.1.0-test-01

--- src/main/java/com/adeuxy/gituserswitch/GitUserProjectState.java
+++ src/main/java/com/adeuxy/gituserswitch/GitUserProjectState.java
@@ -5,12 +5,15 @@ import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.components.Service;
 
+
+@Service(Service.Level.PROJECT)
 @State(
         name = "GitUserProjectState",
         storages = @Storage("gitUserProjectState.xml")
 )
-public class GitUserProjectState implements PersistentStateComponent<GitUserProjectState.StateData> {
+public final class GitUserProjectState implements PersistentStateComponent<GitUserProjectState.StateData> {
     public static class StateData {
         public Integer fixedUserIndex = null;
         public boolean fixed = false;
@@ -21,27 +24,36 @@ public class GitUserProjectState implements PersistentStateComponent<GitUserProj
     @Nullable
     @Override
     public StateData getState() {
+        if (state == null) state = new StateData();
         return state;
     }
 
     @Override
     public void loadState(@NotNull StateData state) {
-        this.state = state;
+        if (state == null) {
+            this.state = new StateData();
+        } else {
+            this.state = state;
+        }
     }
 
     public Integer getFixedUserIndex() {
+        if (state == null) state = new StateData();
         return state.fixedUserIndex;
     }
 
     public void setFixedUserIndex(Integer idx) {
+        if (state == null) state = new StateData();
         state.fixedUserIndex = idx;
     }
 
     public boolean isFixed() {
+        if (state == null) state = new StateData();
         return state.fixed;
     }
 
     public void setFixed(boolean fixed) {
+        if (state == null) state = new StateData();
         state.fixed = fixed;
     }
 } 
